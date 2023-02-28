c2b3a6be59b373ca57481bb57c4d22e6d9e7bfba
Udit <uthakkar@gammastack.com>
2023-02-28 15:15:20 +0530

    finishing up

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,22 @@
 class CommentsController < ApplicationController
 
+  http_basic_authenticate_with name: "user", password: "1234", except: [:index, :show]
   def create
     @movie = Movie.find(params[:movie_id])
     @comment = @movie.comments.create(comment_params)
     redirect_to movie_path(@movie)
   end
 
+  def destroy
+    movie = Movie.find(params[:movie_id])
+    comment = movie.comments.find(params[:id])
+    comment.destroy
+    redirect_to movie_path(movie), status: :see_other
+  end
+
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:commenter, :body, :status)
     end
 
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,12 @@
 class MoviesController < ApplicationController
+  http_basic_authenticate_with name: "user", password: "1234", except: [:index, :show]
   def index
     @movies = Movie.all
   end
   def show
     @movie = Movie.find(params[:id])
   end
-  
+
   def new
     @movie = Movie.new
   end
@@ -42,6 +43,6 @@ class MoviesController < ApplicationController
 
   private
     def movie_params
-      params.require(:movie).permit(:title, :body)
+      params.require(:movie).permit(:title, :body, :status)
     end
 end
