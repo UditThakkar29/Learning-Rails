d5b8ea0e55cc9b774a961ec5ab28ef3ba014334c
Udit <uthakkar@gammastack.com>
2023-03-01 18:14:16 +0530

    temp

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,14 @@
 class Movie < ApplicationRecord
   include Visible
   has_many :comments, dependent: :destroy
+  # before_save :update_movie_name
 
   validates :title, presence: true
   validates :body, presence: true, length: {minimum: 5}
+
+  # private
+  #   def update_movie_name
+  #     self.title.downcase!
+  #   end
 
 end
